8e3fe9ad195b06c346422f24910379d8f6b515fd
Chris Buckett <chrisbuckett@gmail.com>
2013-02-12 11:26:55 +0000

    Added template and phone list to code and html

--- web/app.dart
+++ web/app.dart
@@ -4,3 +4,16 @@ import 'package:web_ui/web_ui.dart';
 main() {
   
 }
+
+List<Phone> phones = [
+    new Phone("Nexus S", "Fast just got faster with Nexus S."),
+    new Phone("Motorola XOOM™ with Wi-Fi","The Next, Next Generation tablet."),
+    new Phone("MOTOROLA XOOM™", "The Next, Next Generation tablet.") 
+  ];
+
+class Phone {
+  String name;
+  String snippet;
+  
+  Phone(this.name, this.snippet);
+}
